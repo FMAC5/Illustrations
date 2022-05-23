1a9995493913ad4628b11823513994cad5b83662
FMAC5 <89349964+FMAC5@users.noreply.github.com>
2022-05-23 15:48:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,105 @@
+
+
+let img1 = document.querySelector('#img1');
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+let btn3 = document.querySelector('#btn3');
+
+btn1.addEventListener ('click', ()=> {
+    img1.src = 'assets/mtfuji_day.png';
+})
+
+btn2.addEventListener ('click', ()=> {
+    img1.src = 'assets/mtfuji_sunset.png';
+})
+
+btn3.addEventListener ('click', ()=> {
+    img1.src = 'assets/mtfuji_night.png';
+})
+
+
+let img2 = document.querySelector('#img2');
+let btn4 = document.querySelector('#btn4');
+let btn5 = document.querySelector('#btn5');
+let btn6 = document.querySelector('#btn6');
+
+btn4.addEventListener ('click', ()=> {
+    img2.src = 'assets/sea_day.png';
+})
+
+btn5.addEventListener ('click', ()=> {
+    img2.src = 'assets/sea_sunset.png';
+})
+
+btn6.addEventListener ('click', ()=> {
+    img2.src = 'assets/sea_night.png';
+})
+
+let img3 = document.querySelector('#img3');
+let btn7 = document.querySelector('#btn7');
+let btn8 = document.querySelector('#btn8');
+let btn9 = document.querySelector('#btn9');
+
+btn7.addEventListener ('click', ()=> {
+    img3.src = 'assets/lighthouse_day.png';
+})
+
+btn8.addEventListener ('click', ()=> {
+    img3.src = 'assets/lighthouse_sunset.png';
+})
+
+btn9.addEventListener ('click', ()=> {
+    img3.src = 'assets/lighthouse_night.png';
+})
+
+let img4 = document.querySelector('#img4');
+let btn10 = document.querySelector('#btn10');
+let btn11 = document.querySelector('#btn11');
+let btn12 = document.querySelector('#btn12');
+
+btn10.addEventListener ('click', ()=> {
+    img4.src = 'assets/tropical_day.png';
+})
+
+btn11.addEventListener ('click', ()=> {
+    img4.src = 'assets/tropical_sunset.png';
+})
+
+btn12.addEventListener ('click', ()=> {
+    img4.src = 'assets/tropical_night.png';
+})
+
+let img5 = document.querySelector('#img5');
+let btn13 = document.querySelector('#btn13');
+let btn14 = document.querySelector('#btn14');
+let btn15 = document.querySelector('#btn15');
+
+btn13.addEventListener ('click', ()=> {
+    img5.src = 'assets/gate_day.png';
+})
+
+btn14.addEventListener ('click', ()=> {
+    img5.src = 'assets/gate_sunset.png';
+})
+
+btn15.addEventListener ('click', ()=> {
+    img5.src = 'assets/gate_night.png';
+})
+
+let img6 = document.querySelector('#img6');
+let btn16 = document.querySelector('#btn16');
+let btn17 = document.querySelector('#btn17');
+let btn18 = document.querySelector('#btn18');
+
+btn16.addEventListener ('click', ()=> {
+    img6.src = 'assets/hill_day.png';
+})
+
+btn17.addEventListener ('click', ()=> {
+    img6.src = 'assets/hill_sunset.png';
+})
+
+btn18.addEventListener ('click', ()=> {
+    img6.src = 'assets/hill_night.png';
+})
+
